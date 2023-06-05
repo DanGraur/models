d6337000596446034be6c63d5d67cce479a65678
DanGraur <dan.graur@inf.ethz.ch>
2023-06-05 16:55:48 +0200

    Fixing bug with parameter access.

--- official/vision/configs/image_classification.py
+++ official/vision/configs/image_classification.py
@@ -53,6 +53,7 @@ class DataConfig(cfg.DataConfig):
   # Keep for backward compatibility.
   aug_policy: Optional[str] = None  # None, 'autoaug', or 'randaug'.
   randaug_magnitude: Optional[int] = 10
+  get_ideal_time = False
 
 
 @dataclasses.dataclass
