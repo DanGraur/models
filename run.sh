a9e493efbc85440dad526c83332c9614fb37a310
DanGraur <dan.graur@inf.ethz.ch>
2023-08-04 16:45:41 +0200

    Added detr support (?)

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-model=${1:-"yolo"}
+model=${1:-"vit"}
 dispatcher_address=${2:-""}
 
 # Prepare the logging directory
@@ -10,8 +10,19 @@ mkdir -p ${log_dir}
 # Prepare the experiment parameters
 additional_parameters=""
 if [ "${model}" == "vit" ]; then
-  experiment="deit_imagenet_pretrain"
+  experiment="vit_imagenet_pretrain"
   executable="official/projects/vit/train.py"
+
+  # Run the experiment
+  python3 ${executable} \
+    --mode=train \
+    --experiment=${experiment} \
+    --model_dir=${log_dir} \
+    --tpu="local" \
+    --log_dir=${log_dir}/absl_out.log | tee ${log_dir}/console.log
+elif [ "${model}" == "detr" ]; then
+  experiment="detr_coco_tfrecord"
+  executable="official/projects/detr/train.py"
 
   # Run the experiment
   python3 ${executable} \
