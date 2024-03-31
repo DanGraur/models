9cd3523e791484162e0e1cc7ffe96227e1a250b3
sepehrMSP <sepehrpourghannad@yahoo.com>
2024-03-31 14:39:24 +0200

    Add support for reading bert from google storage

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 model=${1:-"dlrm"}
 batchsz=${2:-16384}
 
+bucket="gs://sepehr-eu-logs/model_garden/bert"
 # 7495680 is a constant for 16 instances per batch
 step_count=$(( 680000 / (${batchsz} / 16) ))
 # Prepare the experiment parameters
@@ -18,7 +19,7 @@ if [ "${model}" == "dlrm" ]; then
   model_config_file=`pwd`"/official/recommendation/ranking/configs/yaml/dlrm_criteo_tpu.yaml"
   params_override="${base_params_override}"
 
-elif [ "${model}" == "bert" ]; then
+elif [ "${model}" == "bert-local" ]; then
   experiment="bert/sentence_prediction_text"
   executable="official/nlp/train.py"
   task_config_file=`pwd`/"official/nlp/configs/experiments/glue_mnli_text.yaml"
@@ -27,6 +28,18 @@ elif [ "${model}" == "bert" ]; then
     task.train_data.vocab_file=`pwd`/../uncased_L-12_H-768_A-12/vocab.txt,
     task.validation_data.tfds_data_dir=`pwd`/../tfds,
     task.validation_data.vocab_file=`pwd`/../uncased_L-12_H-768_A-12/vocab.txt,
+    trainer.train_steps=2,
+    trainer.validation_steps=4,
+    ${base_params_override}
+    "
+
+elif [ "${model}" == "bert-remote" ]; then
+  experiment="bert/sentence_prediction"
+  executable="official/nlp/train.py"
+  task_config_file=`pwd`/"official/nlp/configs/experiments/glue_mnli_matched.yaml"
+  model_config_file=`pwd`/"official/nlp/configs/models/bert_en_uncased_base.yaml"
+  params_override="task.train_data.input_path=${bucket}/mnli_train.tf_record,
+    task.validation_data.input_path=${bucket}/mnli_eval.tf_record,
     trainer.train_steps=2,
     trainer.validation_steps=4,
     ${base_params_override}
@@ -41,7 +54,7 @@ log_dir=logs/${model}/$(date +%F_%H_%M_%S_%3N)
 mkdir -p ${log_dir}
 
 # Run the experiment
-python3 ${executable} \
+taskset -c 0-47 python3 ${executable} \
   --mode=train \
   --experiment=${experiment} \
   --config_file=${task_config_file} \
